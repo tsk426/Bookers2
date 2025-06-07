22b1061928b00e8d1d75b1f052971f4b7a5b5453
EC2 Default User <ec2-user@ip-172-31-8-170.ap-northeast-1.compute.internal>
2025-06-07 16:59:35 +0000

    [finish]6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,22 +2,28 @@ class BookCommentsController < ApplicationController
 
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      @book_comment = BookComment.new
+      respond_to do |format|
+        format.js 
+      end
+    end
   end
 
   def destroy
-    comment = BookComment.find(params[:id])
-    if comment.user == current_user
-      comment.destroy
+    @comment = BookComment.find(params[:id])
+    if @comment.user == current_user
+      @comment.destroy
+      respond_to do |format|
+        format.js
       flash[:success] = "コメントを削除しました"
+    end
     else
       flash[:error] = "他人のコメントは削除できません"
     end
-    redirect_back fallback_location: root_path
   end
 
   private
